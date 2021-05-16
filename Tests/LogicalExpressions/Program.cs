0ed741e654e865f4783a5d6a578b5aed372f71b4
Isi <ismayil.ssmilee.9@gmail.com>
2021-05-16 20:58:20 +0400

    0000012

---
+++ Tests/LogicalExpressions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+
+namespace LogicalExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            // bool isTrue = true;
+            // bool isFalse = false;
+
+            double x = 3.0d * (1.0 / 3.0);
+            // float y = 3.0f * (1.0 / 3.0);
+
+
+            if (x == 1.0)
+            {
+                WriteLine("Success");
+            }
+            else
+            {
+                WriteLine("Failture");
+            }
+            // if (y == 1.0)
+            // {
+            //     WriteLine("Success2");
+            // }
+            // else
+            // {
+            //     WriteLine("Failture2");
+            // }
+
+        }
+    }
+}
