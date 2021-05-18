11ea859ce12713c6bb221b7a8ae119f65fc667b8
Isi <ismayil.ssmilee.9@gmail.com>
2021-05-18 14:33:16 +0400

    ~

--- YellowBook/3/Methods/Program.cs
+++ YellowBook/3/Methods/Program.cs
@@ -5,6 +5,12 @@ namespace Methods
 {
     class Program
     {
+<<<<<<< HEAD
+=======
+        const double MAX_WIDTH = 5.0;
+        const double MIN_WIDTH = 0.5;
+
+>>>>>>> TryClasses
         static void Main(string[] args)
         {
             // Console.WriteLine("Hello World!");
